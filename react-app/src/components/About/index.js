bb623456fd5331f776d629ef44ca2b148afe5441
Brennan Cota <brennancota@yahoo.com>
2023-03-24 16:45:04 -0700

    added styling to about

--- react-app/src/components/About/index.js
+++ react-app/src/components/About/index.js
@@ -4,41 +4,50 @@ const About = () => {
   return (
     <>
       <div className="about-container">
-        <div className="what-is">What is ImageSpace?</div>
-        <div className="about-block">
-          <div className="about-desc">
-            A community doing good
-            <div className="about-header">
-              ImageSpace is a global online marketplace, where people come
-              together to make, sell, buy, and collect unique items. We’re also
-              a community pushing for positive change for small businesses,
-              people, and the planet.
-            </div>
+        <div className="link-header">
+          <div
+            className="bottom-nav-item"
+            onClick={() => alert("Feature coming soon")}
+          >
+            About
           </div>
-          <div className="about-desc">
-            Support independent creators
-            <div className="about-header">
-              {" "}
-              There’s no ImageSpace warehouse – just millions of people selling
-              the things they love. We make the whole process easy, helping you
-              connect directly with makers to find something extraordinary.
-            </div>
+          <div
+            className="bottom-nav-item"
+            onClick={() => alert("Feature coming soon")}
+          >
+            Jobs
           </div>
-          <div className="about-desc">
-            {" "}
-            Peace of mind
-            <div className="about-header">
-              {" "}
-              Your privacy is the highest priority of our dedicated team. And if
-              you ever need assistance, we are always ready to step in for
-              support.
-            </div>
+          <div
+            className="bottom-nav-item"
+            onClick={() => alert("Feature coming soon")}
+          >
+            Blog
+          </div>
+          <div
+            className="bottom-nav-item"
+            onClick={() => alert("Feature coming soon")}
+          >
+            Developers
+          </div>
+          <div
+            className="bottom-nav-item"
+            onClick={() => alert("Feature coming soon")}
+          >
+            Guidelines
+          </div>
+          <div
+            className="bottom-nav-item"
+            onClick={() => alert("Feature coming soon")}
+          >
+            Help
+          </div>
+          <div
+            className="bottom-nav-item"
+            onClick={() => alert("Feature coming soon")}
+          >
+            Report Abuse
           </div>
         </div>
-        <div className="have-question"></div>
-      </div>
-      <div className="links-container">
-        <div className="link-header">Contributer Links</div>
         <div className="links">
           Brennan Cota:&nbsp;{" "}
           <a href="https://github.com/Sleightttt" target="_blank">
