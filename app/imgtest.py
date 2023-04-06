125f7d3feaf39c8305b5580f0d12329483da73f5
Brennan Cota <brennancota@yahoo.com>
2023-04-06 08:05:10 -0700

    tiny styling changes

--- app/imgtest.py
+++ app/imgtest.py
@@ -2,5 +2,5 @@ imgList = ['https://images.unsplash.com/photo-1679345506039-c4228a79c93a?ixlib=r
            'https://images.unsplash.com/photo-1678662542431-935d32c2aae7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80',
            'https://images.unsplash.com/photo-1678662543244-51054fbe49e0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80',
            'https://images.unsplash.com/photo-1678657184870-6efc15adaf9b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80',
-           'https://images.unsplash.com/photo-1678954129535-9f0d186570ea?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2532&q=80'
+           'https://images.unsplash.com/photo-1678954129535-9f0d186570ea?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2532&q=80',
            ]
