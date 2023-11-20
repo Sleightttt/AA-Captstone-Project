d018c04ba328dc231566ac5bca8690fd81c1e7bc
Sleightttt <brennancota@yahoo.com>
2023-11-20 13:37:02 -0800

    fixed image name displaying peroid

--- app/seeds/images.py
+++ app/seeds/images.py
@@ -11,20 +11,23 @@ fake = Faker()
 def seed_images():
     users = User.query.all()
     for i in range(60):
+        fake_text = fake.text(max_nb_chars=20)
+
+        name = fake_text[:-1] if fake_text.endswith('.') else fake_text
+
         image = Image(
-            name=f"{fake.text(max_nb_chars=20)}",
-            description=f"This is the description for Image {i+1}",
+            name=name,
+            description=f"This is the description for Image {i + 1}",
             lat=round(randint(1, 300) / 100, 2),
             lng=round(randint(1, 300) / 100, 2),
             created_at=datetime.utcnow(),
             updated_at=datetime.utcnow(),
-            owner_id=users[randint(1, len(users)-1)].id,
+            owner_id=users[randint(1, len(users) - 1)].id,
             url=f"{imgList[randint(1, len(imgList) - 1)]}"
         )
         db.session.add(image)
 
     db.session.commit()
-
 
 def undo_images():
     if environment == "production":
